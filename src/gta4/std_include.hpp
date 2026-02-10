53c108f828eb58905a2ef90b52e790f024f5ef5f
#xoxor4d <r4d0xzz@live.de>
2026-02-10 15:19:06 +0100

    bump version to 1.2.0

--- src/gta4/std_include.hpp
+++ src/gta4/std_include.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 constexpr auto COMP_MOD_VERSION_MAJOR = 1;
-constexpr auto COMP_MOD_VERSION_MINOR = 1;
-constexpr auto COMP_MOD_VERSION_PATCH = 9;
+constexpr auto COMP_MOD_VERSION_MINOR = 2;
+constexpr auto COMP_MOD_VERSION_PATCH = 0;
 
 //#define LOG_SHADERPRESETS
 
